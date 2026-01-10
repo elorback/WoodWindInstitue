e7cb1ab671139b542daebde24cb2cb0dfd61f36b
elorback <elorback@gmail.com>
2026-01-10 15:10:04 -0800

    added edits to pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Container, Row, Col, Card, Button, CardBody } from 'react-bootstrap';
+//import { Container, Row, Col, Card, Button,  } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 import './css/Home.css'; // Import the CSS file
 
 const Home: React.FC = () => {
     return (
         <>
             <Container className="home-container">
-                <Card>
-                    <Card.Title>Still Working on this part :) </Card.Title>
-                    <Card.Body>Please be patient</Card.Body>
+                <Card className='card-container'>
+                    <Card.Title className='c-title'>Still Working on this part! </Card.Title>
+                    <Card.Body className='c-body'>Please be patient</Card.Body>
                 </Card>
             </Container>
         </>
