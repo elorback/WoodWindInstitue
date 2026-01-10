09021410f162fbea646a03ab61a3aee86c3625c2
elorback <elorback@gmail.com>
2026-01-10 14:42:49 -0800

    updated home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,15 @@
 import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, CardBody } from 'react-bootstrap';
 import './css/Home.css'; // Import the CSS file
 
 const Home: React.FC = () => {
     return (
         <>
             <Container className="home-container">
-                <Row>
-                    <Col md={4}>
-                        <Card className="shadow-sm mb-4">
-                            <Card.Body>
-                                <Card.Title>Feature 1</Card.Title>
-                                <Card.Text>Description of the first feature.</Card.Text>
-                                <Button variant="primary">Learn More</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={4}>
-                        <Card className="shadow-sm mb-4">
-                            <Card.Body>
-                                <Card.Title>Feature 2</Card.Title>
-                                <Card.Text>Description of the second feature.</Card.Text>
-                                <Button variant="primary">Learn More</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={4}>
-                        <Card className="shadow-sm mb-4">
-                            <Card.Body>
-                                <Card.Title>Feature 3</Card.Title>
-                                <Card.Text>Description of the third feature.</Card.Text>
-                                <Button variant="primary">Learn More</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+                <Card>
+                    <Card.Title>Still Working on this part :) </Card.Title>
+                    <Card.Body>Please be patient</Card.Body>
+                </Card>
             </Container>
         </>
     );
